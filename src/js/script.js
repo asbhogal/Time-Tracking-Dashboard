364af2b9e3162c5c6c7b4209b9d8e7e701eaebc3
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-03 16:31:50 +0000

    imported json data to main script

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+import * as dashboardData from "./data.json";
+
+console.log(dashboardData);
+
+const returnDashboardData = "";
+
+/*fetch(`${dashboardData}`, {
+
+    method: "GET",
+
+}).then((Response) => {
+
+    if(Response.ok) {
+        Response.json().then((json) => {
+            console.log(json);
+        })
+    }
+
+}).catch((error) => {
+
+    console.log(error);
+
+});*/

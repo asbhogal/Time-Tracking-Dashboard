1f846fbf077c172b5b457e4cc9f410260a108fea
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-04 17:11:54 +0000

    further adjustments to data mainpulation and script content

--- dist/index.e82f28a0.js
+++ dist/index.e82f28a0.js
@@ -534,66 +534,14 @@ function hmrAcceptRun(bundle, id) {
 },{}],"dV6cC":[function(require,module,exports) {
 var _dataJson = require("./data.json");
 const wordCardSection = document.getElementById("word-card"), playCardSection = document.getElementById("play-card"), studyCardSection = document.getElementById("study-card"), exerciseCardSection = document.getElementById("exercise-card"), socialCardSection = document.getElementById("social-card"), selfCareCardSection = document.getElementById("self-care-card");
-// set up loops to walk through values and return each to respective DOM element
-// use if to check if DOm element matches ID, then insert data
-// console.log(dashboardData);
-// console.log(dashboardData[0]);
-// document.body.innerHTML = JSON.stringify(dashboardData);
-/* DATA TO EXTRAPOLATE IN EACH OBJECT:
-
-*/ for(let individualData in _dataJson){
+for(let individualData in _dataJson){
     // console.log(dashboardData[individualData].timeframes);
     const { daily , weekly , monthly  } = _dataJson[individualData].timeframes;
+    const dailyFigures = Object.keys(daily);
+    console.log(dailyFigures);
     //console.log(daily);
     console.log(daily.current);
-} /*dashboardData.forEach(Object => {
-    // console.log(Object);
-    if (Object.title === 'Work') {
-
-        console.log(Object.timeframes);
-        let { daily, weekly, monthly } = Object.timeframes;
-        //console.log(daily, weekly, monthly);
-
-    } else if (Object.title === 'Play') {
-
-        let { daily, weekly, monthly } = Object.timeframes;
-
-        //console.log(Object.timeframes);
-    } else if (Object.title === 'Study') {
-
-
-        //console.log(Object.timeframes);
-    } else if (Object.title === 'Exercise') {
-
-
-        //console.log(Object.timeframes);
-    } else if (Object.title === 'Social') {
-
-
-        //console.log(Object.timeframes);
-    } // else console.log(Object.timeframes);
-});*/  /*for (let i = 0; i < dashboardData.length; i++) {
-
-    console.log(dashboardData[i]);
-
-}*/  // const returnDashboardData = "";
- /*fetch(`${dashboardData}`, {
-
-    method: "GET",
-
-}).then((Response) => {
-
-    if(Response.ok) {
-        Response.json().then((json) => {
-            console.log(json);
-        })
-    }
-
-}).catch((error) => {
-
-    console.log(error);
-
-});*/  /* TOGGLE BETWEEN DAILY, WEEKLY AND MONTHLY */ 
+}
 
 },{"./data.json":"bhKGh"}],"bhKGh":[function(require,module,exports) {
 module.exports = JSON.parse('[{"title":"Work","timeframes":{"daily":{"current":5,"previous":7},"weekly":{"current":32,"previous":36},"monthly":{"current":103,"previous":128}}},{"title":"Play","timeframes":{"daily":{"current":1,"previous":2},"weekly":{"current":10,"previous":8},"monthly":{"current":23,"previous":29}}},{"title":"Study","timeframes":{"daily":{"current":0,"previous":1},"weekly":{"current":4,"previous":7},"monthly":{"current":13,"previous":19}}},{"title":"Exercise","timeframes":{"daily":{"current":1,"previous":1},"weekly":{"current":4,"previous":5},"monthly":{"current":11,"previous":18}}},{"title":"Social","timeframes":{"daily":{"current":1,"previous":3},"weekly":{"current":5,"previous":10},"monthly":{"current":21,"previous":23}}},{"title":"Self Care","timeframes":{"daily":{"current":0,"previous":1},"weekly":{"current":2,"previous":2},"monthly":{"current":7,"previous":11}}}]');
--- src/js/script.js
+++ src/js/script.js
@@ -7,82 +7,15 @@ const   wordCardSection = document.getElementById('word-card'),
         socialCardSection = document.getElementById('social-card'),
         selfCareCardSection = document.getElementById('self-care-card');
 
-// set up loops to walk through values and return each to respective DOM element
-// use if to check if DOm element matches ID, then insert data
-
-// console.log(dashboardData);
-// console.log(dashboardData[0]);
-
-// document.body.innerHTML = JSON.stringify(dashboardData);
-
-/* DATA TO EXTRAPOLATE IN EACH OBJECT:
-
-*/
-
  for (let individualData in dashboardData) {
 
     // console.log(dashboardData[individualData].timeframes);
 
     const { daily, weekly, monthly } = dashboardData[individualData].timeframes;
 
+    const dailyFigures = Object.keys(daily);
+
+    console.log(dailyFigures);
     //console.log(daily);
     console.log(daily.current);
 } 
-
-
-/*dashboardData.forEach(Object => {
-    // console.log(Object);
-    if (Object.title === 'Work') {
-
-        console.log(Object.timeframes);
-        let { daily, weekly, monthly } = Object.timeframes;
-        //console.log(daily, weekly, monthly);
-
-    } else if (Object.title === 'Play') {
-
-        let { daily, weekly, monthly } = Object.timeframes;
-
-        //console.log(Object.timeframes);
-    } else if (Object.title === 'Study') {
-
-
-        //console.log(Object.timeframes);
-    } else if (Object.title === 'Exercise') {
-
-
-        //console.log(Object.timeframes);
-    } else if (Object.title === 'Social') {
-
-
-        //console.log(Object.timeframes);
-    } // else console.log(Object.timeframes);
-});*/
-
-/*for (let i = 0; i < dashboardData.length; i++) {
-
-    console.log(dashboardData[i]);
-
-}*/
-
-// const returnDashboardData = "";
-
-/*fetch(`${dashboardData}`, {
-
-    method: "GET",
-
-}).then((Response) => {
-
-    if(Response.ok) {
-        Response.json().then((json) => {
-            console.log(json);
-        })
-    }
-
-}).catch((error) => {
-
-    console.log(error);
-
-});*/
-
-
-/* TOGGLE BETWEEN DAILY, WEEKLY AND MONTHLY */

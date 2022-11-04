5ba46b5afbe87ac425dd94b1ca8451ab563cbf59
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-04 16:37:26 +0000

    started destructuring nested objects in array - JSON

--- dist/index.e82f28a0.js
+++ dist/index.e82f28a0.js
@@ -536,15 +536,22 @@ var _dataJson = require("./data.json");
 const wordCardSection = document.getElementById("word-card"), playCardSection = document.getElementById("play-card"), studyCardSection = document.getElementById("study-card"), exerciseCardSection = document.getElementById("exercise-card"), socialCardSection = document.getElementById("social-card"), selfCareCardSection = document.getElementById("self-care-card");
 // set up loops to walk through values and return each to respective DOM element
 // use if to check if DOm element matches ID, then insert data
-console.log(_dataJson);
-console.log(_dataJson[0]);
+// console.log(dashboardData);
+// console.log(dashboardData[0]);
 // document.body.innerHTML = JSON.stringify(dashboardData);
 /* DATA TO EXTRAPOLATE IN EACH OBJECT:
 
-*/ /* for (let individualData in dashboardData) {
-    console.log(individualData);
-} */ for(let i = 0; i < _dataJson.length; i++)console.log(_dataJson[i]);
-const returnDashboardData = ""; /*fetch(`${dashboardData}`, {
+*/ for(let individualData in _dataJson){
+    // console.log(dashboardData[individualData].timeframes);
+    const { daily , monthly , weekly  } = _dataJson[individualData].timeframes;
+    console.log(daily, weekly, monthly);
+    console.log(daily[0]);
+} /*for (let i = 0; i < dashboardData.length; i++) {
+
+    console.log(dashboardData[i]);
+
+}*/  // const returnDashboardData = "";
+ /*fetch(`${dashboardData}`, {
 
     method: "GET",
 
--- src/js/script.js
+++ src/js/script.js
@@ -10,8 +10,8 @@ const   wordCardSection = document.getElementById('word-card'),
 // set up loops to walk through values and return each to respective DOM element
 // use if to check if DOm element matches ID, then insert data
 
-console.log(dashboardData);
-console.log(dashboardData[0]);
+// console.log(dashboardData);
+// console.log(dashboardData[0]);
 
 // document.body.innerHTML = JSON.stringify(dashboardData);
 
@@ -19,17 +19,20 @@ console.log(dashboardData[0]);
 
 */
 
-/* for (let individualData in dashboardData) {
-    console.log(individualData);
-} */
+ for (let individualData in dashboardData) {
+    // console.log(dashboardData[individualData].timeframes);
+    const { daily, monthly, weekly } = dashboardData[individualData].timeframes;
+    console.log(daily, weekly, monthly);
+    console.log(daily[0]);
+} 
 
-for (let i = 0; i < dashboardData.length; i++) {
+/*for (let i = 0; i < dashboardData.length; i++) {
 
     console.log(dashboardData[i]);
 
-}
+}*/
 
-const returnDashboardData = "";
+// const returnDashboardData = "";
 
 /*fetch(`${dashboardData}`, {
 
